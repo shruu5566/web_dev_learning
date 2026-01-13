844edd8a107376c3e0f927de04db8cf6dd855fa9
SHRUSHTI <shrushtibp@gmail.com>
2026-01-13 23:39:01 +0530

    intendation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.getElementById("btn").addEventListener("click", function () {
   const nameInput = document.getElementById("nameInput");
   const output = document.getElementById("output");
-
   if (nameInput.value.trim() === "") {
     output.style.color = "red";
     output.innerText = "Please enter your name 🙂";
