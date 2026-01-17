6c3b67bb937c3054c2307aaf1318785a780c7453
SHRUSHTI <shrushtibp@gmail.com>
2026-01-17 22:59:05 +0530

    add client-side validation for login form

--- mern journey/day-01-html-css-js/script.js
+++ mern journey/day-01-html-css-js/script.js
@@ -1,3 +1,14 @@
-document.querySelector("button").addEventListener("click", () => {
-  alert("Login button clicked!");
+const form=document.getElementById("loginForm");
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  const email=document.getElementById("email").value;
+  const password=document.getElementById("password").value;
+
+  if (password.length<6){
+    alert("password must be at least 6 characters");
+    return;
+  }
+  alert("login successful")
 });
